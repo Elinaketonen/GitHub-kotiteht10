105695a92a370317603d3d08033962d7161c84b7
Elinaketonen <elina.ketonen@metropolia.fi>
2021-02-08 19:17:39 +0200

    Tiedostojen lisäys
    Include

--- Github-testi.php
+++ Github-testi.php
@@ -4,8 +4,10 @@
     <title>GitHubin testailua</title>
 </head>
 <body>
+<?php include 'header.php';?>
  <h1> Pitäisi kerrata html-/css-kurssin sisältöjä, kun ei ole tullut koodattua tyylimääreitä kurssin jälkeen.</h1>
  <p> Seuraavaksi testaan julkaista tämän tiedoston. </p>
+<?php include 'footer.php';?>
     
 </body>
 </html>
